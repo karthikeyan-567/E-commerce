91a0cea96123be4cd23e1f04c0c691828517d0f4
karthikeyan-567 <karthikeyan672005@gmail.com>
2025-02-20 08:45:56 +0530

    e-mmoerce updated

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
-const container = document.querySelector(".slider")
+let indexn = 0;
+let slides = document.getElementsByClassName("slide");
+let lbtn = document.querySelector(".left");
+let rbtn = document.querySelector(".right");
+let slider = document.querySelector(".slider");
 
-let btn_l= document.querySelector(".btn-left") 
-let btn_r = document.querySelector(".btn-right") 
-const ImageList = ["ph-2","ph-3","ph-4"]
-let index =2;
-btn_l.addEventListener("click",function(){
-    console.log("left")
-    if(btn_l.classList.contains("btn-left")){
-        --index;
-        console.log(index)
-    
-    if(index<=0){
-        index = ImageList.length
-    }
+function showSlide(i) {
+  if (i >= slides.length) {
+    indexn = 0;
+  }
+  if (i < 0) {
+    indexn = slides.length - 1;
+  }
+  slider.style.transform = `translateX(${-indexn * 1200}px)`;
 }
-})
-btn_r.addEventListener("click",function(){
-    if(btn_r.classList.contains("btn-right")){
-        index++;
-        console.log(index)
-    
-    if(index<Image.length){
-        index = 0
-    }
-    container.style.background = ' background: url(/images/ph-${ImageList[index]}.jpg) center/cover fixed no-repeat '
+lbtn.addEventListener("click", () => {
+  indexn++;
+  showSlide(indexn);
+});
+rbtn.addEventListener("click", () => {
+  indexn--;
+  showSlide(indexn);
+});
+
+let addCLose = document.querySelector(".addClose");
+let add = document.querySelector("#ADD");
+addCLose.addEventListener("click", () => {
+  add.remove();
+});
+let favun = document.querySelectorAll(".favW");
+let favch = document.querySelectorAll(".favR");
+
+for (let i = 0; i < favun.length; i++) {
+  favun[i].addEventListener("click", () => {
+    favch[i].style.zIndex = "1";
+    favch[i].style.zIndex = "5";
+  });
 }
-})
+for (let j = 0; j < favch.length; j++) {
+  favch[j].addEventListener("click", () => {
+    favch[j].style.zIndex = "1";
+    favun[j].style.zIndex = "5";
+  });
+}
+
+window.onload = function () {
+  setTimeout(function () {
+    document.querySelector(".popup").style.display = "flex";
+    document.querySelector(".overlay").style.display = "block";
+  }, 3000);
+};
+function closePopup() {
+  document.querySelector(".popup").style.display = "none";
+  document.querySelector(".overlay").style.display = "none";
+}
